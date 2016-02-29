a0a57bdc7ee90e41ac1295b982700f7afd24a7d9
Luke Hedger <luke.hedger@gmail.com>
2016-02-29 16:57:42 +0000

    Setup stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": "stylelint-config-standard",
+  "rules": {
+    "function-calc-no-unspaced-operator": null
+  },
+  "ignoreFiles": [
+    "source/css/shared/reset.css"
+  ]
+}

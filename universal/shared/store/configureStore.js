84c2d7c8b7722f10be5b50119bca352133bce94d
Luke Hedger <luke.hedger@gmail.com>
2016-06-29 18:04:34 +0100

    enable loggers only in development mode

--- universal/shared/store/configureStore.js
+++ universal/shared/store/configureStore.js
@@ -1,24 +1,32 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import { browserHistory } from 'react-router';
-import { routerMiddleware } from 'react-router-redux';
-import createLogger from 'redux-logger';
-import rootReducer from '../reducers';
+import { createStore, applyMiddleware, compose } from 'redux'
+import { browserHistory } from 'react-router'
+import { routerMiddleware } from 'react-router-redux'
+import createLogger from 'redux-logger'
+import rootReducer from '../reducers'
 
-const logger = createLogger();
-const router = routerMiddleware(browserHistory);
+// middleware
+const router = routerMiddleware(browserHistory)
+
+let middleware = [ router ]
+
+// logger middleware in development
+if (process.env.NODE_ENV === 'development') {
+  const logger = createLogger({ collapsed: true })
+  middleware.push(logger)
+}
 
 const finalCreateStore = compose(
-  applyMiddleware(router, logger),
-  window.devToolsExtension ? window.devToolsExtension() : f => f
-)(createStore);
+  applyMiddleware(...middleware),
+  window.devToolsExtension && process.env.NODE_ENV === 'development' ? window.devToolsExtension() : f => f
+)(createStore)
 
 // Grab the state from a global injected into server-generated HTML
-const initialState = window.__INITIAL_STATE__ || {};
+const initialState = window.__INITIAL_STATE__ || {}
 
 export default function configureStore(state = initialState) {
 
-  const store = finalCreateStore(rootReducer, initialState);
+  const store = finalCreateStore(rootReducer, initialState)
 
-  return store;
+  return store
 
 }

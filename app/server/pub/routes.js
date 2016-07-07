fd39cae5128f9f4f0e1f513695d2ade800c4996c
Luke Hedger <luke.hedger@gmail.com>
2016-07-07 17:39:26 +0100

    disable pub CUD routes for now

--- app/server/pub/routes.js
+++ app/server/pub/routes.js
@@ -3,9 +3,9 @@ import * as controller from './controller'
 
 const router = express.Router()
 
-router.post( '/', controller.create, (req, res, next) => next() )
+// router.post( '/', controller.create, (req, res, next) => next() )
 router.get( '/:id', controller.read, (req, res, next) => next() )
-router.put( '/:id', controller.update, (req, res, next) => next() )
-router.delete( '/:id', controller.remove, (req, res, next) => next() )
+// router.put( '/:id', controller.update, (req, res, next) => next() )
+// router.delete( '/:id', controller.remove, (req, res, next) => next() )
 
 export default router

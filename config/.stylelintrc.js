772c5daadc9d157cbe75bb43a1dded901f9b4e23
lukehedger <luke.hedger@gmail.com>
2016-09-26 19:41:36 +0100

    updated stylelint and fixed issues

--- config/.stylelintrc.js
+++ config/.stylelintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   "extends": "stylelint-config-standard",
   "rules": {
-    "function-calc-no-unspaced-operator": null
+    "function-calc-no-unspaced-operator": null,
+    "selector-pseudo-class-no-unknown": [ true, { ignorePseudoClasses: [ "global" ] } ],
+    "string-quotes": "single",
   },
   "ignoreFiles": [
     "../app/client/css/shared/reset.css"

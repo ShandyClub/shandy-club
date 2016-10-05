da73fc6cc613e25f1317598a876093f16e31a230
Luke Hedger <luke.hedger@gmail.com>
2016-10-05 17:26:37 +0100

    add some pub details

--- app/universal/shared/components/pub.js
+++ app/universal/shared/components/pub.js
@@ -1,14 +1,22 @@
 import React from 'react'
-// import { avatar, features, subtitle, title } from './'
+import uuid from 'node-uuid'
 import styles from 'css/components/pub.css'
 
 const Pub = ({ address, features, name, postcode }) => {
 
-  console.log(address, features, name, postcode)
+  const renderFeatures = features.keySeq().map( f => features.get(f) ? (
+    <div key={uuid.v4()}>{ f }</div>
+  ) : null )
 
   return (
     <div className={styles.base}>
-      Pub
+
+      <h4>{ name }</h4>
+
+      <h6>{ address } { postcode }</h6>
+
+      { renderFeatures }
+      
     </div>
   )
 

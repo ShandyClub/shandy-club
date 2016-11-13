38eeb29396cece94f1fa21ab8648a5a3945fa255
lukehedger <luke.hedger@gmail.com>
2016-11-13 15:38:34 +0000

    revamp colours

--- app/universal/shared/style/colours.js
+++ app/universal/shared/style/colours.js
@@ -1,9 +1,23 @@
 /**
-  * @desc Colour palette
+  * @desc Colour palette and utilities
 */
+
+import Color from 'color'
+
+// -----
+// PALETTE
+// -----
 
 export const dark = '#47444F'
 export const light = '#FFFBF3'
 export const shandy = '#F58C40'
 export const accent = '#27A5F9'
 export const error = '#AC6F76'
+
+// -----
+// UTILS
+// -----
+
+export const rgb = colour => colour.rgbString()
+export const alpha = (colour, value) => rgb( Color(colour).alpha(value) )
+export const lighten = (colour, value) => rgb( Color(colour).lighten(value) )

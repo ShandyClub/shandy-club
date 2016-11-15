caebd63dda58a3c3e8358586d4442649be2b5a02
Luke Hedger <luke.hedger@gmail.com>
2016-11-15 11:13:53 +0000

    setup custom analytics events for search actions

--- app/universal/search/actions.js
+++ app/universal/search/actions.js
@@ -5,17 +5,33 @@ export const getGeocode = term => ({
   type: actions.GEOCODE_REQUEST,
   payload: {
     term
-  }
+  },
+  meta: {
+    analytics: {
+      type: 'geocodeRequest',
+      payload: { term },
+    },
+  },
 })
 
 export const geocodeSuccess = payload => ({
   type: actions.GEOCODE_SUCCESS,
   payload,
+  meta: {
+    analytics: {
+      type: 'geocodeSuccess',
+    },
+  },
 })
 
 export const geocodeFailure = payload => ({
   type: actions.GEOCODE_FAILURE,
   payload,
+  meta: {
+    analytics: {
+      type: 'geocodeFailure',
+    },
+  },
 })
 
 export const setGeocode = (point, term) => ({
@@ -23,7 +39,13 @@ export const setGeocode = (point, term) => ({
   payload: {
     term,
     point
-  }
+  },
+  meta: {
+    analytics: {
+      type: 'geocodeSelect',
+      payload: { point, term },
+    },
+  },
 })
 
 export const clearGeocode = () => ({
@@ -35,31 +57,60 @@ export const clearGeocode = () => ({
 })
 
 export const getGeolocation = () => ({
-  type: actions.GEOLOCATION_REQUEST
+  type: actions.GEOLOCATION_REQUEST,
+  meta: {
+    analytics: {
+      type: 'geolocationRequest',
+    },
+  },
 })
 
 export const geolocationSuccess = payload => ({
   type: actions.GEOLOCATION_SUCCESS,
   payload,
+  meta: {
+    analytics: {
+      type: 'geolocationSuccess',
+      payload: { geolocation: payload.geolocation },
+    },
+  },
 })
 
 export const geolocationFailure = payload => ({
   type: actions.GEOLOCATION_FAILURE,
   payload,
+  meta: {
+    analytics: {
+      type: 'geolocationFailure',
+    },
+  },
 })
 
 export const setPoint = (point=DEFAULT_POINT) => ({
   type: actions.POINT_SET,
   payload: {
     point
-  }
+  },
+  meta: {
+    analytics: {
+      type: 'mapDragSearch',
+      payload: { point },
+    },
+  },
 })
 
 export const setSelectedResult = (selectedResultIndex=null) => ({
   type: actions.SELECTED_RESULT_SET,
   payload: {
     selectedResultIndex,
-  }
+  },
+  meta: {
+    analytics: {
+      type: 'setSelectedResult',
+      // TODO - method! marker or panel
+      // payload: { method },
+    },
+  },
 })
 
 export const toggleFeature = feature => ({
@@ -70,29 +121,44 @@ export const toggleFeature = feature => ({
   meta: {
     analytics: {
       type: 'toggleFeature',
-      payload: {
-        feature,
-      }
+      payload: { feature },
     },
   },
 })
 
 export const submitSearch = meta => ({
   type: actions.SUBMIT_REQUEST,
+  // TODO - analytics meta w/search method
   meta,
 })
 
 export const submitSuccess = (payload, meta) => ({
   type: actions.SUBMIT_SUCCESS,
   payload,
-  meta,
+  meta: {
+    ...meta,
+    analytics: {
+      type: 'submitSuccess',
+      payload: { results: payload.results.length },
+    },
+  },
 })
 
 export const submitFailure = payload => ({
   type: actions.SUBMIT_FAILURE,
   payload,
+  meta: {
+    analytics: {
+      type: 'submitFailure',
+    },
+  },
 })
 
 export const resetSearch = () => ({
-  type: actions.RESET
+  type: actions.RESET,
+  meta: {
+    analytics: {
+      type: 'resetSearch',
+    },
+  },
 })

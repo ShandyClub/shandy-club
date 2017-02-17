bdb8ce7061f27f37abffc4617a13f9b5d9914fc1
Luke Hedger <luke.hedger@gmail.com>
2017-02-17 16:51:46 +0000

    update font on server render html

--- app/server/index.js
+++ app/server/index.js
@@ -36,7 +36,7 @@ const renderPage = (html, initialState) => `
   <title>Shandy Club</title>
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no" media="(device-height: 568px)" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/10up-sanitize.css/4.1.0/sanitize.min.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:700|Open+Sans">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:700|Karla">
   <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.0.1/dist/leaflet.css" />
   <link rel="shortcut icon" href="/favicon.png">

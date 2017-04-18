1bdeb408d76428d4455e50c4b7e6b6762b68d7dc
lukehedger <luke.hedger@gmail.com>
2017-04-18 15:17:37 +0100

    revert prettier write

--- app/core/middleware/index.js
+++ app/core/middleware/index.js
@@ -1,23 +1,24 @@
-import analyticsMiddleware from 'redux-analytics';
-import createLogger from 'redux-logger';
-import {middleware as pack} from 'redux-pack';
+import analyticsMiddleware from 'redux-analytics'
+import createLogger from 'redux-logger'
+import { middleware as pack } from 'redux-pack'
 
-import {track} from '../util/track';
+import { track } from '../util/track'
 
 const createMiddleware = isDevelopment => {
+
   // default middleware
-  let middleware = [pack];
+  let middleware = [ pack ]
 
   // logger middleware in development
-  if (isDevelopment) middleware.push(createLogger({collapsed: true}));
+  if (isDevelopment) middleware.push(createLogger({ collapsed: true }))
 
   // analytics middleware in production
-  if (!isDevelopment)
-    middleware.push(
-      analyticsMiddleware(({type, payload}) => track(type, payload)),
-    );
+  if (!isDevelopment) middleware.push(
+    analyticsMiddleware( ({type, payload }) => track(type, payload) )
+  )
 
-  return middleware;
-};
+  return middleware
 
-export default createMiddleware;
+}
+
+export default createMiddleware
